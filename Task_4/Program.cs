7f2af6ef402f13e9b69bcb466372e864cdbd991e
Savodyarkin Nikita <snikitafut@mail.ru>
2023-11-21 18:31:55 +0300

    task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,24 @@
+﻿int num = 654564645;
+
+int length = Math.Abs(num).ToString().Length;
+
+var array = new int [length];
+
+int count = 0;
+
+int ostatok;
+
+while (num > 0) {
+    ostatok = num % 10;
+    num = num / 10;
+    array[count] = ostatok;
+    //Console.WriteLine(array[count]);
+    count++;
+}
+
+Array.Reverse(array);
+
+
+
+
+Console.WriteLine($"{string.Join(", ", array)}");
