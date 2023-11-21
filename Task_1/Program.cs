ee0de485f8159ddc68082b14cff828e2a0f45071
Savodyarkin Nikita <snikitafut@mail.ru>
2023-11-21 17:20:51 +0300

    Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,8 @@
+﻿int num = 322;
+ 
+
+if((num % 7 == 0) && (num % 23 == 0) ) {
+    Console.WriteLine($"Число {num} кратно 23 и 7");
+} else {
+    Console.WriteLine($"Число {num} не кратно 23 и 7");
+}
