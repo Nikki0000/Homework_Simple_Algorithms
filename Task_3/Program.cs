db3cd28acd8c54f98fef53af62c98bbfe62c89fd
Savodyarkin Nikita <snikitafut@mail.ru>
2023-11-21 17:39:20 +0300

    task 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,13 @@
+﻿int num = 21;
+
+int first = num /10;
+int second = num % 10;
+
+if (first > second) {
+    Console.WriteLine(first);
+} else {
+    Console.WriteLine(second);
+}
+
+// Console.WriteLine(first);
+// Console.WriteLine(second);
