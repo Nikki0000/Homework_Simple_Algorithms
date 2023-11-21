819680a45097b2670915430f3a16a3539f9ffa0e
Savodyarkin Nikita <snikitafut@mail.ru>
2023-11-21 17:34:28 +0300

    task 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,12 @@
+﻿int x = 2;
+int y = 3;
+
+if (x > 0 & y > 0) {
+    Console.WriteLine("1");
+} else if (x < 0 & y > 0) {
+    Console.WriteLine("2");
+} else if (x < 0 & y < 0) {
+    Console.WriteLine("3");
+} else {
+    Console.WriteLine("4");
+}
